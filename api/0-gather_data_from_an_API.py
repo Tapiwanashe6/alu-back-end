6ef90d97029eca82c32a3f03f756f1016ac5f1b5
Tapiwanashe6 <t.marufu@alustudent.com>
2025-02-21 02:56:57 -0800

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,27 @@
+#!/usr/bin/python3
+"""
+Using a REST API, and a given emp_ID, return info about their TODO list.
+"""
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    # URL for the API to get users and their todos
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    
-    # Fetch user data
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("User not found.")
-        return
-    
-    user_data = user_response.json()
-    employee_name = user_data['name']
-
-    # Fetch todos data
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculate completed tasks
-    completed_tasks = [todo['title'] for todo in todos_data if todo['completed']]
-    number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos_data)
-
-    # Display the result
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-    
-    try:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-    except ValueError:
-        print("Please provide a valid integer for employee ID.")
+    """ main section """
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee = requests.get(
+        BASE_URL + f'/users/{sys.argv[1]}/').json()
+    EMPLOYEE_NAME = employee.get("name")
+    employee_todos = requests.get(
+        BASE_URL + f'/users/{sys.argv[1]}/todos').json()
+    serialized_todos = {}
+
+    for todo in employee_todos:
+        serialized_todos.update({todo.get("title"): todo.get("completed")})
+
+    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, COMPLETED_LEN, len(serialized_todos)))
+    for key, val in serialized_todos.items():
+        if val is True:
+            print("\t {}".format(key))
